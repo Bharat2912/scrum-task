3f9746b9ce1c2bcd96255f1cdf70fc8c45a6f7b0
Bharat Prajapati <53166316+Bharat2912@users.noreply.github.com>
2023-10-02 22:32:01 +0530

    Update Header.js

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>Scrum Task</h1>
+            <h1>Scrum Task Updated</h1>
         </div>
     )
 }
