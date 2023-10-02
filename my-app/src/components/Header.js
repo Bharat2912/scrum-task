14874a8ee870e742f606374cdcd5a08637868943
Bharat Prajapati <53166316+Bharat2912@users.noreply.github.com>
2023-10-02 23:06:33 +0530

    revertedHeader

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return(
         <div className="header">
-            <h1>Scrum Task Updated BooM</h1>
+            <h1>Scrum Task reverted</h1>
         </div>
     )
 }
